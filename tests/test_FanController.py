b302d4ff930bd84ee4354679ffac9a5ff476712e
Jamie Sage <jamie@jamiesage.co.uk>
2022-07-30 12:21:00 +0100

    Initial work on basic unit tests for each facade

---
+++ tests/test_FanController.py
@@ -0,0 +1,48 @@
+import pytest
+from facades.FanController import FanController
+from facades.IPMITool import IPMITool
+
+@pytest.fixture
+def ipmitool(mocker):        
+    def mockVerifyCredentails(self):
+        pass
+        
+    mocker.patch(
+        'facades.IPMITool.IPMITool.verifyCredentails',
+        mockVerifyCredentails
+    )
+    
+    def mockExecute(self, command):
+        return ("stdout", "stderr")
+
+    mocker.patch(
+        'facades.IPMITool.IPMITool.execute',
+        mockExecute
+    )
+
+    return IPMITool('127.0.0.1', 'root', 'calvin')
+
+
+@pytest.fixture
+def controller(mocker, ipmitool):
+    return FanController(ipmitool)
+
+def test_set_manual_mode(mocker, controller):
+    controller.setManualMode()
+    assert controller.mode == "manual"
+    assert controller.getMode() == "manual"
+
+def test_set_static_fan_mode(mocker, controller):
+    controller.setStaticFanMode()
+    assert controller.mode == "static"
+    assert controller.getMode() == "static"
+
+def test_set_speed(mocker, controller):
+    controller.setSpeed(15)
+    assert controller.speed == 15
+    assert controller.getSpeed() == 15
+
+def test_set_speed_static(mocker, controller):
+    controller.setSpeed('static')
+    assert controller.mode == "static"
+    assert controller.getMode() == "static"
---
+++ tests/test_TabbedTable.py
@@ -0,0 +1,37 @@
+import pytest
+
+from facades.parsers.TabbedTable import TabbedTable
+
+def tests_tabbed_table_get():
+    table = TabbedTable("""
+        Foo    |    Bar
+        Zoo    |    123   
+        Bob    |    123    |   789
+    """)
+    assert table.get('Foo') == "Bar"
+    assert table.get('Zoo') == "123"
+    assert type(table.get('Bob')) is list
+    assert table.get('Bob')[0] == "123"
+    assert table.get('Bob')[1] == "789"
+
+def tests_tabbed_table_get_none():
+    table = TabbedTable("""
+        Foo    |    Bar
+        Zoo    |    123   
+    """)
+    assert table.get('Bad') is None
+
+def tests_tabbed_table_get_bad_format():
+    table = TabbedTable("""
+        Foo
+        Zoo    |    123   
+    """)
+    assert table.get('Zoo') == "123"
+    assert table.get('Foo') is None
+
+def tests_tabbed_table_all():
+    table = TabbedTable("""
+        Foo    |    Bar
+        Zoo    |    123   
+    """)
+    assert type(table.all()) is list
